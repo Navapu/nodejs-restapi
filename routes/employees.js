a97d4f734abaddc7dc0584762877b25dd08d165e
Navapu <drojannp2@gmail.com>
2023-02-21 12:20:30 +0100

    Modularizacion de las rutas

---
+++ routes/employees.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/employees', function(req, res, next) {
+    res.send("Obteniendo empleados");
+  });
+router.post('/employees', function(req, res, next) {
+    res.send("Añadiendo empleados");
+});
+router.put('/employees', function(req, res, next) {
+    res.send("Actualizando empleados");
+});
+router.delete('/employees', function(req, res, next) {
+    res.send("Eliminando empleados");
+});
+
+
+  module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pool= require('../db.js')
+const employeesRoutes=require("./employees")
 
-/* GET home page. */
 router.get('/ping', async function(req, res, next) {
   const [result] = await pool.query('SELECT 1+1 as result')
   res.json(result);
@@ -10,18 +10,7 @@ router.get('/ping', async function(req, res, next) {
 router.get('/', function(req, res, next) {
   res.send("Hola mundo");
 });
-router.get('/employees', function(req, res, next) {
-  res.send("Obteniendo empleados");
-});
-router.post('/employees', function(req, res, next) {
-  res.send("Añadiendo empleados");
-});
-router.put('/employees', function(req, res, next) {
-  res.send("Actualizando empleados");
-});
-router.delete('/employees', function(req, res, next) {
-  res.send("Eliminando empleados");
-});
 
+router.use(employeesRoutes)
 
 module.exports = router;
