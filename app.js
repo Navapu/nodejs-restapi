8e92bed16bace5d5499adcf4267125c5dcab44e1
Navapu <drojannp2@gmail.com>
2023-02-21 16:37:43 +0100

    APP finalizada

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/api', employeesRouter);
 
+app.use((req,res,next)=>{
+    res.status(404).json({message: "Ruta no encontrada"})
+})
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -1,63 +1,101 @@
 const pool= require('../db.js')
 
 const getEmployees = async function(req, res, next) {
-    const [rows]= await pool.query('SELECT * FROM employee')
-    // res.send("Obteniendo empleados")
-    res.json(rows)
+    try {
+        // throw new Error("Error porque si")
+        const [rows]= await pool.query('SELECT * FROM employee')
+        // res.send("Obteniendo empleados")
+        res.json(rows)
+    } catch (error) {
+        return res.status(500).json({
+            message:"Algo ha ido mal"
+        })
+    }
 }
 
 const getEmployee= async (req,res)=>{
-    console.log(req.params.id)
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
-    console.log(rows)
+    try {
+        // throw new Error("Error porque si")
 
-    if (rows.length<=0){
-        return res.status(404).json({
-            message: 'Empleado no encontrado'
-        })
-    }
+        console.log(req.params.id)
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+        console.log(rows)
+
+        if (rows.length<=0){
+            return res.status(404).json({
+                message: 'Empleado no encontrado'
+            })
+        }
 
     res.json(rows[0])
+    } catch (error) {
+        return res.status(500).json({
+            message:"Algo ha ido mal"
+        })
+    }
 }
 
 const postEmployee = async function(req, res, next) {
+    try {
+        // throw new Error("Error porque si")
 
-    const {name,salary}=req.body
-    const [rows]= await pool.query('INSERT INTO employee (name,salary) VALUES (?,?)',[name,salary])
-    console.log(req.body)
-    res.send({
-        id: rows.insertId,
-        name,
-        salary
-    })
+        const {name,salary}=req.body
+        const [rows]= await pool.query('INSERT INTO employee (name,salary) VALUES (?,?)',[name,salary])
+        console.log(req.body)
+        res.send({
+            id: rows.insertId,
+            name,
+            salary
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Algo ha ido mal"
+        }) 
+    }
 }
 
 const putEmployee = async function(req, res, next) {
-    const {id} = req.params
-    const {name, salary} = req.body
-    const [rows] = await pool.query('UPDATE employee SET NAME = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?',[name,salary,id])
-    if (rows.affectedRows<=0) {
-        return res.status(404).json({
-            message: 'Empleado no encontrado'
-        })
-    }
-    const [result] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+    try {
+        // throw new Error("Error porque si")
 
-    res.json(result[0])
+        const {id} = req.params
+        const {name, salary} = req.body
+        const [rows] = await pool.query('UPDATE employee SET NAME = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?',[name,salary,id])
+        if (rows.affectedRows<=0) {
+            return res.status(404).json({
+                message: 'Empleado no encontrado'
+            })
+        }
+        const [result] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+    
+        res.json(result[0])
+    } catch (error) {
+        return res.status(500).json({
+            message:"Algo ha ido mal"
+        })    
+    }
 
 }
 
 const deleteEmployee = async function(req, res, next) {
-    const [rows] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
-    console.log(rows)
+    try {
+        // throw new Error("Error porque si")
 
-    if (rows.affectedRows<=0) {
-        return res.status(404).json({
-            message: 'Empleado no encontrado'
+        const [rows] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
+        console.log(rows)
+    
+        if (rows.affectedRows<=0) {
+            return res.status(404).json({
+                message: 'Empleado no encontrado'
+            })
+        }
+    
+        res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message:"Algo ha ido mal"
         })
     }
-
-    res.sendStatus(204)
 }
 
 module.exports={
